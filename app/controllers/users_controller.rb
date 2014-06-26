8607c374fd94ea55ff573c5d9137ef407eb83fcb
jeffreytakaki <jeffreytakaki@Jeffreys-MacBook-Pro.local>
2014-06-25 22:18:02 -0700

    push to heroku 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,15 +35,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # def destroy
-  #   @user = user.find(params[:id])
-  #   @user.destroy
-  #   respond_to do |format|
-  #     format.html {redirect_to :root}
-  #     format.json {render json: {head: :ok}}
-  #   end
-  # end
-
   protected
 
   def user_params
@@ -52,66 +43,3 @@ class UsersController < ApplicationController
 end
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# class UsersController < ApplicationController
-# 	before_action  :only => [:show, :edit, :update, :destroy]
-	
-# 	def index
-# 		@users = User.all
-# 	end
-
-# 	def new
-# 		@user = User.new
-
-# 	end
-
-# 	def create
-# 		@user = User.new(user_params)
-# 		if @user.save
-# 			flash[:success] = "Thank you for signing up!"
-# 			session[:remember_token] = @user.id
-# 			@current_user = @user
-# 			redirect_to @user
-# 		else
-# 			render :new
-# 		end
-# 	end
-
-# 	def show
-# 		@user = User.find(params[:id])
-# 		@donut = Donut.new
-# 	end
-
-
-# 	# yelp search api commented out due to scope limitations.
-# 	# def search(location, search_term)
-#     # parameters = { term: params[:term], limit: 8 }
-#        # render :json => Yelp.client.search(location, term: "donuts",limit: 20).businesses
-#        # @name = Yelp.client.search(location, term: "donuts",radius_filter: 10,limit: 20).businesses
-#     # @city_state_zip = Yelp.client.search(location, term: search_term,limit: 8).businesses[0].location.display_address[2]
-#     # @phone = Yelp.client.search(location, term: search_term,limit: 8).businesses[0].phone
-#     # @web = Yelp.client.search(location, term: search_term,limit: 8).businesses[0].url
-#   	# end
-
-# 	protected
-
-# 	def user_params
-#     	params.require(:user).permit(:email, :name, :password)
-#   	end
-# end
